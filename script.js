3c58c41ad1641a8d2043a22a7e7053de71eb28f7
Stdv <stanisalvdv@gmail.com>
2021-01-08 18:10:38 +0300

    13 lesson

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже посмтрели?", "");
+// const numberOfFilms = prompt("Сколько фильмов вы уже посмтрели?", "");
+//
+// const personalMovieDB = {
+//             count: numberOfFilms,
+//             movies: {},
+//             actors: {},
+//             genres: [],
+//             privat: false
+// };
+//
+// const   a = prompt("Один из последних просмотренных фильмов?", ""),
+//         b = prompt("На сколько оцените его?", ""),
+//         c = prompt("Один из последних просмотренных фильмов?", ""),
+//         d = prompt("На сколько оцените его?", "");
+//
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+//
+// console.log(personalMovieDB);
 
-const personalMovieDB = {
-            count: numberOfFilms,
-            movies: {},
-            actors: {},
-            genres: [],
-            privat: false
-};
 
-const   a = prompt("Один из последних просмотренных фильмов?", ""),
-        b = prompt("На сколько оцените его?", ""),
-        c = prompt("Один из последних просмотренных фильмов?", ""),
-        d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// if (x<60) {
+//     console.log('No')
+// } else if (x>100) {
+//     console.log('More')
+// } else {
+//     console.log('Yes')
+// }
+//
+// (x <= 60) ? console.log('Yes') : console.log('No');
+
+// const x = '60';
+//
+// switch (x) {
+//     case '49':
+//         console.log ('No');
+//         break;
+//     case '50':
+//         console.log('No');
+//         break;
+//     case '60':
+//         console.log('Yes');
+//         break;
+//     default:
+//         console.log('Увы');
+//         break;
+// }
+
